31e0ebd42f398830040e176f38db63443bb7a771
orbot <tang4937@gmail.com>
2019-02-13 17:46:59 +0800

    Create 嗯哼你懂的.js

---
+++ 嗯哼你懂的.js
@@ -0,0 +1,15 @@
+auto("fast");
+
+toast("plase open liaotianchuangkou");
+sleep(500);
+
+while(true){
+if(currentPackage() == "com.stardust.scriptdroid"){
+sleep(8000);
+continue;
+
+}
+setText(" 开始你的邪恶操作")                                                                                                                                                                             出                                                                                                来                                                                                                                                                                                                   送妈");
+click("发送");
+//然后你妈的就可以无限循环了啊哈哈哈哈哈，什么狗屁多线程你们可以研究一下哈
+}
